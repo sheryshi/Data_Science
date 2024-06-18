e244fa740609778f41181c530c02cf671b1eada9
shreyashi singh <sheryshisingh772@gmail.com>
2024-06-18 23:16:43 +0530

    first commit

--- Api.py
+++ Api.py
@@ -1,15 +1,19 @@
-import http.client
+import requests
 
-conn = http.client.HTTPSConnection("api-nba-v1.p.rapidapi.com")
+url = "https://fresh-linkedin-profile-data.p.rapidapi.com/search-decision-makers"
 
+payload = {
+	"company_ids": ["163640", "19080118"],
+	"title_keywords": ["CEO", "Founder", "Owner"],
+	"geo_codes": [103644278, 102299470],
+	"limit": "25"
+}
 headers = {
-    'x-rapidapi-key': "919d42aad8msh141809834802b44p1ca286jsne4d8c87fcfa7",
-    'x-rapidapi-host': "api-nba-v1.p.rapidapi.com"
+	"x-rapidapi-key": "919d42aad8msh141809834802b44p1ca286jsne4d8c87fcfa7",
+	"x-rapidapi-host": "fresh-linkedin-profile-data.p.rapidapi.com",
+	"Content-Type": "application/json"
 }
 
-conn.request("GET", "/players/statistics?game=8133", headers=headers)
+response = requests.post(url, json=payload, headers=headers)
 
-res = conn.getresponse()
-data = res.read()
-
-print(data.decode("utf-8"))
+print(response.json())
